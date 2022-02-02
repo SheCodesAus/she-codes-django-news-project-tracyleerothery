a7faf61748085728c760985b96c56588e2616c4f
Tracy Rothery <tracy.rothery@gmail.com>
2022-02-02 19:48:03 +0800

    update

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,21 +1,33 @@
 from django.views import generic
 from .models import NewsStory
 
+from django.urls import reverse_lazy
+from .forms import StoryForm
+
 
 class IndexView(generic.ListView):
-    template_name = 'news/index.html'
+    template_name = "news/index.html"
 
     def get_queryset(self):
-        '''Return all news stories.'''
+        """Return all news stories."""
         return NewsStory.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
-        context['all_stories'] = NewsStory.objects.all()
+        context["latest_stories"] = NewsStory.objects.all()[:4]
+        context["all_stories"] = NewsStory.objects.all()
         return context
+
 
 class StoryView(generic.DetailView):
     model = NewsStory
-    template_name = 'news/story.html'
-    context_object_name = 'story'
+    template_name = "news/story.html"
+    context_object_name = "story"
+
+
+class AddStoryView(generic.CreateView):
+    form_class = StoryForm
+    context_object_name = "storyForm"
+    template_name = "news/createStory.html"
+    success_url = reverse_lazy("news:index")
+
