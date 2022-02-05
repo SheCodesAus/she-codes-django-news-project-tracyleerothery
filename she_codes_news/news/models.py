63127782e1cb3142d23b80ecdac30031fe57137e
Tracy Rothery <tracy.rothery@gmail.com>
2022-02-05 12:04:22 +0800

    update_news_story

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    # author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
     pub_date = models.DateTimeField()
     content = models.TextField()
