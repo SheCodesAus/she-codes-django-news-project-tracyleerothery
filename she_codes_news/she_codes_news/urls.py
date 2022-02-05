ac2621bab2eb936efedef6a60ed85899f660a38c
Tracy Rothery <tracy.rothery@gmail.com>
2022-02-05 12:48:39 +0800

    update_create_user_profile

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from users.views import ProfileView
+
 
 
 urlpatterns = [
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
+    path('user/<int:pk>', ProfileView.as_view(), name="userprofile"),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,4 +1,12 @@
+from re import template
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
-class 
+from django.contrib.auth import get_user_model
+
+USER_MODEL = get_user_model()
+
+class ProfileView(generic.DetailView):
+    model = USER_MODEL
+    template_name = 'users/profile.html'
+    context_object_name = 'user'
