810d21e94c0ef8c88d482f18f8ad19f2b6ef4ee6
Tracy Rothery <tracy.rothery@gmail.com>
2022-02-08 20:27:08 +0800

    update_user_story
    update that lets users see their story to their user profile

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,8 +1,15 @@
 from re import template
+
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from django.contrib.auth import get_user_model
+
+
+from .forms import CustomUserCreationForm
+
+from news.models import NewsStory
 
 USER_MODEL = get_user_model()
 
@@ -10,3 +17,15 @@ class ProfileView(generic.DetailView):
     model = USER_MODEL
     template_name = 'users/profile.html'
     context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['user_story'] = NewsStory.objects.filter(author=self.kwargs['pk'])
+            print(context['user_story'])
+            return context
+
+    
+class CreateAccountView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "users/createAccount.html"
