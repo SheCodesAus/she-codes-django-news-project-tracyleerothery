8ed03f1936a029d8eb87763b07e916147aef494c
Tracy Rothery <tracy.rothery@gmail.com>
2022-02-15 17:28:38 +0800

    update_category

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -11,14 +11,25 @@ class NewsStory(models.Model):
         on_delete=models.CASCADE
     )
 
+
     pub_date = models.DateTimeField()
     # image_url = models.URLField(default='https://picsum.photos/600')
     content = models.TextField()
     category = models.ForeignKey("news.Category", on_delete=models.CASCADE, null = True, blank = True)
+    image=models.URLField(null=True, blank=True)
 
+    class Meta:
+        ordering = ['-pub_date']
     
     def __str__ (self):
         return self.title
+    
+# categories = (
+#     ("NEWS", "News"),
+#     ("WEATHER", "Weather"),
+# )
+
+# category = models.CharField(max_length=200, choices= categories, default="News")
 
 
 class Category(models.Model):
